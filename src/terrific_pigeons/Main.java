8d73aa8d7ff1f509c931a1abdfe96785f91f4dbf
szijjas <61512935+szijjas@users.noreply.github.com>
2020-03-28 10:35:55 +0100

    szasz hori

--- src/terrific_pigeons/Main.java
+++ src/terrific_pigeons/Main.java
@@ -1,11 +1,39 @@
 package terrific_pigeons;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        //szasz hori
-        //csá
+        Eskimo e;
+        Tile t1,t2;
+
+        System.out.println("Menu");
+        System.out.println("1.Eskimo moves to stable tile");
+        System.out.println("2.Eskimo moves to unstable tile");
+        //lehet akk itt meg kene kerdezni h tobben allnak e rajta mint amennyit elbir
+        System.out.println("3.Eskimo drowns");
+        System.out.println("4.Eskimo freezes");
+        System.out.println("5.Eskimo uses diving suit");
+        System.out.println("6.Eskimo eats food");
+        System.out.println("7.Eskimo assembles pistol");
+        System.out.println("8.Eskimo saves with rope");
+        System.out.println("9.Eskimo builds iglu");
+        System.out.println("10.Researcher scans tile");
+        System.out.println("11.Eskimo digs");
+        System.out.println("12.Eskimo pass");
+        System.out.println("13.Eskimo equips thing");
+        System.out.println("14.Snowstorm catches player");
+        System.out.println("15.Snowstorm comes, player safe in iglu\n");
+
+        System.out.print("Please choose: ");
+        Scanner input = new Scanner( System.in );
+        int choice = input.nextInt();
+
+
+
+
+
     }
 
 }
