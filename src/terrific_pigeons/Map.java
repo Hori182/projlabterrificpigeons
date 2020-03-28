ccb50bde4f3b5b8872f42e0e950e05ef55aa0ec7
Áron Horváth <horvatharon15@gmail.com>
2020-03-28 12:35:47 +0100

    unstable komment

--- src/terrific_pigeons/Map.java
+++ src/terrific_pigeons/Map.java
@@ -4,14 +4,21 @@ public class Map {
 
     private Tile[] tiles;
 
-    /**/
+    /*
+    * ??
+    * */
     public void snowStorm() {}
 
-    /**/
+    /*
+    * Visszaadja a tile-okat, amik a Map-ot alkotjak.
+    * */
     public Tile[] getTiles() {
         return tiles;
     }
-    /**/
+    /*
+    * Beállítja a tiles tömb értékét.
+    * @param tiles : Ezzel írja felül a tiles értékét.
+    * */
     public void setTiles(Tile[] tiles) {
         this.tiles = tiles;
     }
