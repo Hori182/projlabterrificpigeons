7cdfec63124b8d76eef41cb1a8247cf65c93c681
Áron Horváth <horvatharon15@gmail.com>
2020-04-24 11:23:42 +0200

    main

--- src/terrific_pigeons/Main.java
+++ src/terrific_pigeons/Main.java
@@ -1,10 +1,25 @@
 package terrific_pigeons;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Number of eskimoes: ");
+        int numOfEskimoes = Integer.parseInt(input.nextLine());
+        System.out.println("Number of reserchers: ");
+        int numOfReserchers = Integer.parseInt(input.nextLine());
+        Game game = new Game();
+        for(int i = 0; i < numOfEskimoes; i++) {
+            Eskimo e = new Eskimo();
+            game.addPlayers(e);
+        }
+        for(int i = 0; i < numOfReserchers; i++) {
+            Researcher r = new Researcher();
+            game.addPlayers(r);
+        }
+
         String command = input.nextLine();
         while(command != "exit")
         {
@@ -13,10 +28,12 @@ public class Main {
                 switch(commandparam[0])
                 {
                     case "load":
-                    {
+
 
                         break;
-                    }
+
+                    case "pass":
+
                 }
             command = input.nextLine();
             commandparam = command.split(" ");
