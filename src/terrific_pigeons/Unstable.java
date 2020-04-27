122347229cd62477a3ec289ab0c6d20fa941605a
Fazekas Arnold <fazekasarn@gmail.com>
2020-04-27 03:58:53 +0200

    .

--- src/terrific_pigeons/Unstable.java
+++ src/terrific_pigeons/Unstable.java
@@ -66,22 +66,22 @@ public class  Unstable extends Tile{
     }
 
     public void save(PrintWriter writer){
-        writer.print(getTileId()+":-:"+limit+":"+(safe?"I:":(safeByTent?"T:":" :"))+snow+":");
+        writer.print(getTileId()+":-:"+limit+":"+(safe?"I:":(safeByTent?"T:":"-:"))+snow+":");
         ArrayList<String> ids = new ArrayList<>();
         for (MoveAble m : movables)
             ids.add(m.getId());
         if (ids.size()>0)
             writer.print(String.join(",", ids) + ":");
         else
-            writer.print(" :");
-        writer.print(thing==null?" :":(thing.Name()+":"));
+            writer.print("-:");
+        writer.print(thing==null?"-:":(thing.Name()+":"));
         ids = new ArrayList<>();
         for (Tile t: neighbours)
             ids.add(String.valueOf(t.getTileId()));
         if (ids.size()>0)
-            writer.println(String.join(",", ids) + ";");
+            writer.println(String.join(",", ids) + ":");
         else
-            writer.println(" ;");
+            writer.println("-:");
 
     }
 }
